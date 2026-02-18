cb462e45d011c7ed2963909dbdca6945c9cad3d2
Jhonatan Homsany <jhonatanhomzuz@gmail.com>
2026-02-18 13:47:01 -0400

    SCH-77: integrando github a jira

--- schedio-frontend/src/main.ts
+++ schedio-frontend/src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-//Comentario de prueba para la integracion de github al jira
+//Comentario de prueba para la integracion de github al jira prueba 2
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
