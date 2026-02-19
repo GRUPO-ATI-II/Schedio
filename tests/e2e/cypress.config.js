8a8ae7e4402bd6ea93ed109756309f20e9251751
Jcova2003 <jesusecova73@gmail.com>
2026-02-19 00:58:36 -0400

    test

--- tests/e2e/cypress.config.js
+++ tests/e2e/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: process.env.CYPRESS_BASE_URL || "http://frontend:4200",
+    baseUrl: "http://localhost:4200",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: false
   }
