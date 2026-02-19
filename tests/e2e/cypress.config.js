ccf81ff900158a571a42976a0d3d646db3d3d2a3
Jcova2003 <jesusecova73@gmail.com>
2026-02-18 22:46:53 -0400

    fix cypress.config.js

--- tests/e2e/cypress.config.js
+++ tests/e2e/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: process.env.CYPRESS_BASE_URL || "http://schedio-frontend",
+    baseUrl: process.env.CYPRESS_BASE_URL || "http://frontend:4200",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: false
   }
