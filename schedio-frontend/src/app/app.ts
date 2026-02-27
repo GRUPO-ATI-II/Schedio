e5918fffe583f7d4f5fe430c5a8f49300feae3ab
Jcova2003 <jesusecova73@gmail.com>
2026-02-26 21:47:04 -0400

    add layout and pages directory

--- schedio-frontend/src/app/app.ts
+++ schedio-frontend/src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { SidebarMenu } from './components/sidebar-menu/sidebar-menu';
-import { MainAppComponent } from './components/main-app/main-app';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, SidebarMenu, MainAppComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
