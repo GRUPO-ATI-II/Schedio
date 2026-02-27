685c191e39ee6c1523e386713b64a73d56afd125
Jcova2003 <jesusecova73@gmail.com>
2026-02-26 23:20:47 -0400

    create login page

--- schedio-frontend/src/app/pages/login/login.ts
+++ schedio-frontend/src/app/pages/login/login.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { InputField } from '../../components/ui/input-field/input-field';
+import { RectBaseButton } from '../../components/ui/rect-base-button/rect-base-button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [InputField, RectBaseButton],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
