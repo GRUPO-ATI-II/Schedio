5525ff5cb6b30f4135633b9d67d3f8059da2f85f
Jcova2003 <jesusecova73@gmail.com>
2026-02-26 23:36:37 -0400

    connect login to tickets page

--- schedio-frontend/src/app/components/ui/rect-base-button/rect-base-button.ts
+++ schedio-frontend/src/app/components/ui/rect-base-button/rect-base-button.ts
@@ -1,13 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-rect-base-button',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './rect-base-button.html',
   styleUrl: './rect-base-button.css'
 })
 export class RectBaseButton {
   @Input() isAction: boolean = false;
+
+  @Output() clicked = new EventEmitter<void>();
+
+  handleClick() {
+    this.clicked.emit();
+  }
 }
--- schedio-frontend/src/app/pages/login/login.ts
+++ schedio-frontend/src/app/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { InputField } from '../../components/ui/input-field/input-field';
 import { RectBaseButton } from '../../components/ui/rect-base-button/rect-base-button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,5 +10,9 @@ import { RectBaseButton } from '../../components/ui/rect-base-button/rect-base-b
   styleUrl: './login.css',
 })
 export class Login {
+  constructor(private router: Router) {}
 
+  login() {
+    this.router.navigate(['/ticket']);
+  }
 }
