c1d2b4742251fe361eb11863b15e5aa4ba222f31
Sofia Marcano <soamaia03@gmail.com>
2026-03-06 01:47:32 -0400

    Added create-event function + page

--- schedio-frontend/src/app/app.routes.ts
+++ schedio-frontend/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { EditSpecificUserCredentials } from './pages/contact-center/edit-specifi
 import { Error } from './pages/error/error';
 import { EditProfile } from './pages/edit-profile/edit-profile';
 import { CreateAssignment } from './pages/create-assignment/create-assignment';
+import { CreateEvent } from './pages/create-event/create-event';
 import { NotFound } from './pages/not-found/not-found';
 import { authGuard } from './core/guards/auth.guard';
 
@@ -56,6 +57,10 @@ export const routes: Routes = [
       {
         path: 'agenda/new-assignment',
         component: CreateAssignment,
+      },
+      {
+        path: 'agenda/new-event',
+        component: CreateEvent,
       },
       {
         path: '**',
