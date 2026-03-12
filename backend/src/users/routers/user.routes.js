5e5bc4fd6dedde341d44a39dc56990544ce023a0
ValCicco <valerria02@gmail.com>
2026-03-11 23:35:30 -0400

    prueba

--- backend/src/users/routers/user.routes.js
+++ backend/src/users/routers/user.routes.js
@@ -5,7 +5,6 @@ const userController = require("../controllers/user.controller");
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.put("/:email", userController.update);
-router.put("/id/:id", userController.updateById);
 router.delete("/:email", userController.remove);
 
 module.exports = router;
