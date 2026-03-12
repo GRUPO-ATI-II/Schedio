bacfd51820b019e2aae1810c55ff56c34dc1dda1
ValCicco <valerria02@gmail.com>
2026-03-11 23:25:39 -0400

    prueba

--- backend/index.js
+++ backend/index.js
@@ -86,7 +86,7 @@ const startApp = async () => {
     const PORT = process.env.PORT || 3000;
     const HOST = '0.0.0.0';
 
-    app.listen(PORT, () => {
+    app.listen(PORT, HOST, () => {
       console.log(
         `🚀 [Server]: Schedio API lista en modo ${env.toUpperCase()}`,
       );
